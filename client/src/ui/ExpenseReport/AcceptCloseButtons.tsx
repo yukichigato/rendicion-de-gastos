996d4756c686b13a16ffef21e71498654c475717
Flauvourr <flauvourr@gmail.com>
2025-02-26 01:32:03 -0300

    Client: Changes to buttons in expense report component

--- client/src/ui/ExpenseReport/AcceptCloseButtons.tsx
+++ client/src/ui/ExpenseReport/AcceptCloseButtons.tsx
@@ -4,7 +4,7 @@ const AcceptCloseButtons = ({ status }: { status: string }) => {
   if (status === "Pending") {
     return (
       <>
-        <button className="rounded-lg border-[.0626rem] border-red-400 bg-red-400 p-2 text-white transition-all duration-200 hover:border-gray-900 hover:bg-white hover:text-gray-900">
+        <button className="rounded-lg border-[.0626rem] border-lime-500 bg-white p-2 text-lime-500 transition-all duration-200 hover:border-gray-900 hover:bg-white hover:text-gray-900">
           Accept
         </button>
         <button className="bg-white-900 rounded-lg border-[.0626rem] border-red-400 p-2 text-red-400 transition-all duration-200 hover:border-gray-900 hover:bg-white hover:text-gray-900">
