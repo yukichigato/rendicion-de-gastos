ab76d25f3a42a893d5bdb22f7261e23662f5fa51
Flauvourr <flauvourr@gmail.com>
2025-03-18 15:48:08 -0300

    Base

--- client-old/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const SALT_ROUNDS = process.env.SALT_ROUNDS;
-export const NEXT_PUBLIC_DB_API_BASEURL =
-  process.env.NEXT_PUBLIC_DB_API_BASEURL;
-export const NEXT_PUBLIC_AUTH_API_BASEURL =
-  process.env.NEXT_PUBLIC_AUTH_API_BASEURL;
