e5e69566c2e3e71fb5775eb6df7d707c9754b7ea
yukichigato <andres.araya780@gmail.com>
2025-03-27 20:54:54 -0300

    Updated docker stuff and small edit to init.sql

--- db_api/init.sql
+++ db_api/init.sql
@@ -38,5 +38,3 @@ CREATE TABLE IF NOT EXISTS expense_report (
 INSERT INTO users ("name", "rut", "password", "tel", "email", "status", "area") 
     VALUES
         ('Yukichi Gato', '12.345.678-9', '$2a$10$g6R5179Mf/zkHfq83Scj..QkRSZ3KrsfHTihowh7fTTzNIBasWH.S', '+56 9 1234 5678', 'yukichi@email.com', 'Admin', 'Otros');
-
-SELECT * FROM users;
